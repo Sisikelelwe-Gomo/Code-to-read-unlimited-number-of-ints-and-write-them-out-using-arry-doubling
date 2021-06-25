37499df0e73a8dab86a590dc15079fdda4bda071
Sisikelelwe Gomo <53611592+Sisikelelwe-Gomo@users.noreply.github.com>
2021-06-25 17:43:30 +0200

    Add files via upload

---
+++ Code to read unlimited number of ints and write them out using arry-doubling.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/* Read an unlimited number of ints with no attempts at error
+recovery; fill the vector parameter with the data; its size
+after the return tells how many items were read.
+*/
+void getInts ( vecotr<int> & array )
+{
+
+    int itemsRead = 0;
+    int inputVal;
+}
